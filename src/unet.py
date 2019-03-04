b862d9d5e5629f9518a64293b46cac80d5dbb384
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-03-04 01:45:52 -0500

    added another layer of batch normalization

--- src/unet.py
+++ src/unet.py
@@ -24,7 +24,6 @@ dl = DataLoader()
 
 def dice_loss_function(y_true, y_pred, smooth=1):
     """
-    This function is implementation of Sørensen–Dice coefficient
     :y_true: ground truth
     :y_pred: prediction
     :smooth: smoothing parameter to negate division by zero error
@@ -38,7 +37,6 @@ def dice_loss_function(y_true, y_pred, smooth=1):
 
 def dice_coefficient(y_true, y_pred):
     """
-    This function is implementation of Sørensen–Dice coefficient
     :y_true: ground truth
     :y_pred: prediction
     :return: the negative of dice_loss_function
@@ -121,74 +119,109 @@ def unet(l2_reg=0.0002, lr=1e-5, kernel_size=3, dropout_rate=0.3, input_shape=(6
     conv1 = BatchNormalization()(conv1)
     conv1 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv1)
     conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv1)
+    conv1 = BatchNormalization()(conv1)
+    
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
     pool1 = Dropout(dropout_rate)(pool1)
+    
     conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(pool1)
     conv2 = BatchNormalization()(conv2)
     conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv2)
     conv2 = BatchNormalization()(conv2)
     conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv2)
     conv2 = BatchNormalization()(conv2)
+    conv2 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv2)
+    conv2 = BatchNormalization()(conv2)
+
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
     pool2 = Dropout(dropout_rate)(pool2)
+    
     conv3 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(pool2)
     conv3 = BatchNormalization()(conv3)
     conv3 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv3)
     conv3 = BatchNormalization()(conv3)
     conv3 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv3)
     conv3 = BatchNormalization()(conv3)
+    conv3 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv3)
+    conv3 = BatchNormalization()(conv3)
+
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     pool3 = Dropout(dropout_rate)(pool3)
+    
     conv4 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(pool3)
     conv4 = BatchNormalization()(conv4)
     conv4 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv4)
     conv4 = BatchNormalization()(conv4)
     conv4 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv4)
     conv4 = BatchNormalization()(conv4)
+    conv4 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv4)
+    conv4 = BatchNormalization()(conv4)
+
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
     pool4 = Dropout(dropout_rate)(pool4)
+    
     conv5 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(pool4)
     conv5 = BatchNormalization()(conv5)
     conv5 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv5)
     conv5 = BatchNormalization()(conv5)
     conv5 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv5)
     conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2D(512, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv5)
+    conv5 = BatchNormalization()(conv5)
+    
     up6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5)
     up6 = concatenate([up6, conv4], name='up6', axis=3)
     up6 = Dropout(dropout_rate)(up6)
+
     conv6 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(up6)
     conv6 = BatchNormalization()(conv6)
     conv6 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv6)
     conv6 = BatchNormalization()(conv6)
     conv6 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv6)
     conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2D(256, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv6)
+    conv6 = BatchNormalization()(conv6)
+
     up7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6)
     up7 = concatenate([up7, conv3], name='up7', axis=3)
     up7 = Dropout(dropout_rate)(up7)
+    
     conv7 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(up7)
     conv7 = BatchNormalization()(conv7)
     conv7 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv7)
     conv7 = BatchNormalization()(conv7)
     conv7 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv7)
     conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2D(128, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv7)
+    conv7 = BatchNormalization()(conv7)
+
     up8 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7)
     up8 = concatenate([up8, conv2], name='up8', axis=3)
     up8 = Dropout(dropout_rate)(up8)
+    
     conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(up8)
     conv8 = BatchNormalization()(conv8)
     conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv8)
     conv8 = BatchNormalization()(conv8)
     conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv8)
     conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2D(64, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv8)
+    conv8 = BatchNormalization()(conv8)
+    
     up9 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8)
     up9 = concatenate([up9, conv1], name='up9', axis=3)
     up9 = Dropout(dropout_rate)(up9)
+
     conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(up9)
     conv9 = BatchNormalization()(conv9)
     conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv9)
     conv9 = BatchNormalization()(conv9)
     conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv9)
     conv9 = BatchNormalization()(conv9)
+    conv9 = Conv2D(32, (kernel_size, kernel_size), activation='relu', padding='same', kernel_regularizer=regularizers.l2(l2_reg))(conv9)
+    conv9 = BatchNormalization()(conv9)
+
     conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
     model = Model(inputs=inputs, outputs=conv10)
     model.compile(optimizer=Adam(lr=lr), loss=dice_loss_function, metrics=[dice_coefficient])
